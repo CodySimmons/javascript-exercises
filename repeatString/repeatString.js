38f4f56a7e89fd4f0975b208d12ee0e8a7ba2fbe
Cody Simmons <cdysmmns@gmail.com>
2021-01-22 12:25:03 -0700

    Exercise 02 completed
    Takes string and amount of loops wanted as function parameters.
    If loop count is <0, returns ERROR.

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,9 @@
 const repeatString = function(inputtedWord, loopCount) {
 
+    if(loopCount<0){
+        return 'ERROR';
+    }
+
     let newString = "";
 
     for (let i = 0; i < loopCount; i++){
