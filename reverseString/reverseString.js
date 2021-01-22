c063695122ac14081fd8f46ee38de837f3659ad5
Cody Simmons <cdysmmns@gmail.com>
2021-01-22 12:55:14 -0700

    Exercise 3 completed.
    Takes variable as input, creates an empty array.
    Loops through input variable, using input length as constraint.
    Pushes string character from ending to beginning into array.
    Returns array with a blank join.

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(userString) {
+    let stringArray = [];
+
+    for (let i = userString.length; i >= 0; i--){
+        stringArray.push(userString.substr(i, 1));
+        // console.log(i);
+        // console.log(userString.substr(i, 1))
+    }
+
+    return stringArray.join('');
 
 }
+
+// console.log(reverseString("Handlebar"));
 
 module.exports = reverseString
