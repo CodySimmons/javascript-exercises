1bc6582637d0594d14c63f8022602c5da502ff1c
Cody Simmons <cdysmmns@gmail.com>
2021-01-23 20:16:51 -0700

    Excercise 7 finished. ftoc return celsius temp rounded to 1st decimal. ctof returns fahrenheit temp rounded to 1st decimal,

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,15 @@
 const ftoc = function() {
-
+  return (Math.round(((arguments[0] - 32) * (5/9)) * 10) / 10);
 }
 
 const ctof = function() {
-
+  return (Math.round(((arguments[0] * 9) / 5 + 32) * 10) / 10);
 }
 
 module.exports = {
   ftoc,
   ctof
 }
+
+console.log(ftoc(100));
+console.log(ctof(32));
