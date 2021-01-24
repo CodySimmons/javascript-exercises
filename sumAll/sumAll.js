f051710691c853565d7f79828c11371d8fc4dbd0
Cody Simmons <cdysmmns@gmail.com>
2021-01-23 18:35:05 -0700

    Exercise 5 completed. Takes 2 inputs, uses them as a range and inclusively adds all numbers between them. Will reject inputs that are not numbers or are less than 0.

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,32 @@
 const sumAll = function() {
+    let firstNumber, secondNumber;
+    let addedNumber = 0;
 
+    if(typeof arguments[0] !== "number" || arguments[0] < 0 || typeof arguments[1] !== "number" || arguments[1] < 0){
+        return 'ERROR'
+    }
+    else{
+        if (arguments[0] < arguments[1]){
+            firstNumber = arguments[0];
+            secondNumber = arguments[1];
+        }
+        else {
+            firstNumber = arguments[1];
+            secondNumber = arguments[0];
+        }
+
+        // console.log(firstNumber);
+        // console.log(secondNumber);
+
+        for (let i = firstNumber; i <= secondNumber; i++){
+            // console.log(i);
+            addedNumber += i;
+        }
+
+        return addedNumber;
+    }
 }
 
 module.exports = sumAll
+
+console.log(sumAll(10, "15"));
