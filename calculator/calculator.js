6481fb5c41d197e1ea0aabe5bdce6f3ab4c30806
Cody Simmons <devCodySimmons@gmail.com>
2021-01-30 15:26:40 -0700

    Completion of calculator exercise.

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,32 +1,55 @@
-function add () {
-	
+function add(a, b) {
+  return a + b;
 }
 
-function subtract () {
-	
+function subtract(a, b) {
+  return a - b;
+}
+function sum(obj) {
+  let sum = 0;
+  //   console.log(obj);
+
+  for (let key of obj) {
+    sum += key;
+  }
+  return sum;
 }
 
-function sum () {
-	
+function multiply(obj) {
+  let multiplyTotal = 1;
+  for (let key of obj) {
+    multiplyTotal *= key;
+  }
+  return multiplyTotal;
 }
 
-function multiply () {
-	
+function power(a, b) {
+  return Math.pow(a, b);
 }
 
-function power() {
-	
-}
-
-function factorial() {
-	
+function factorial(x) {
+  if (x === 0) {
+    return 1;
+  }
+  let factorialArray = [];
+  for (let i = 1; i <= x; i++) {
+    factorialArray.push(i);
+  }
+  return multiply(factorialArray);
 }
 
 module.exports = {
-	add,
-	subtract,
-	sum,
-	multiply,
-    power,
-	factorial
-}
+  add,
+  subtract,
+  sum,
+  multiply,
+  power,
+  factorial,
+};
+
+// let sumArray = [1, 2, 3, 4, 5, 6, 7, 8];
+// let sumArray = [];
+// console.log(sum(sumArray));
+// console.log(sum([7]));
+// console.log(multiply([8, 2]));
+console.log(factorial(5));
