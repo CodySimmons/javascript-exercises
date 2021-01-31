4a91eb58f8bb24308f449b641c803412223838f0
Cody Simmons <devCodySimmons@gmail.com>
2021-01-30 18:49:37 -0700

    getTheTitles exercise completed.

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,16 @@
-const getTheTitles = function() {
-
-}
+const getTheTitles = function (obj) {
+  return obj.map((book) => book.title);
+};
 
 module.exports = getTheTitles;
+
+// const books = [
+//   {
+//     title: "Book",
+//     author: "Name",
+//   },
+//   {
+//     title: "Book2",
+//     author: "Name2",
+//   },
+// ];
