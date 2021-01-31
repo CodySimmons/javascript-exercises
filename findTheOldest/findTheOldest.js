247dca0b2100fbfe46e88d6c4b24a4e6ddbd5ec5
Cody Simmons <devCodySimmons@gmail.com>
2021-01-30 19:26:26 -0700

    findTheOldest exercise completed.

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,43 @@
-let findTheOldest = function() {
+const findTheOldest = function (array) {
+  // const oldestSort = obj.sort((a, b) =>
+  //   a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth ? -1 : 1
+  // );
+  // return oldestSort[0];
+  return (temp = array.reduce((oldest, currentPerson) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(
+      currentPerson.yearOfBirth,
+      currentPerson.yearOfDeath
+    );
+    return oldestAge < currentAge ? currentPerson : oldest;
+  }));
+};
 
-}
+const getAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+};
 
-module.exports = findTheOldest
+module.exports = findTheOldest;
+
+// const people = [
+//   {
+//     name: "Carly",
+//     yearOfBirth: 1942,
+//     yearOfDeath: 1970,
+//   },
+//   {
+//     name: "Ray",
+//     yearOfBirth: 1962,
+//     yearOfDeath: 2011,
+//   },
+//   {
+//     name: "Jane",
+//     yearOfBirth: 1912,
+//     yearOfDeath: 1941,
+//   },
+// ];
+
+// console.log(findTheOldest(people));
